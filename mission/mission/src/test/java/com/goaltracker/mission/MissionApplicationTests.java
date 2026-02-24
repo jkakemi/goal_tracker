3a8603b903fa421b771f0359fa2be791cb65ae0c
jkakemi <julia.koba@ufu.br>
2026-02-24 15:29:03 -0300

    create: new project

---
+++ mission/mission/src/test/java/com/goaltracker/mission/MissionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.goaltracker.mission;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MissionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
